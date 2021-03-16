536de6f76dae5fb0d9de13ea65cf7e650b132bac
aospiridonov <aospiridonov@gmail.com>
2021-03-16 21:53:59 +0300

    the structure of a good test

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -3,7 +3,13 @@ import 'package:test/test.dart';
 
 void main() {
   test('the calculator returns 4 when adding 2 and 2', () {
+    // 1st step: setup -> create the calculator object
     final calculator = Calculator();
-    expect(calculator.add(2, 2), 4);
+
+    //2nd step: side effects -> collect the result you want test
+    final result = calculator.add(2, 2);
+
+    //3rd step: expectations -> compare the result against an excpected value
+    expect(result, 4);
   });
 }
