85819afdb4a67aa812f35f6a71b830c2dd30a54c
aospiridonov <aospiridonov@gmail.com>
2021-03-16 21:48:53 +0300

    simple unit test in dart

---
+++ lib/src/calculator.dart
@@ -0,0 +1,6 @@
+class Calculator {
+  double add(double a, double b) => a + b;
+  double substact(double a, double b) => a - b;
+  double multiply(double a, double b) => a * b;
+  double divide(double a, double b) => a / b;
+}
