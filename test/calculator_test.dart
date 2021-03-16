71d07aac9d69c9bea0db86f85063bd2f1d1e2406
aospiridonov <aospiridonov@gmail.com>
2021-03-16 21:57:51 +0300

    add reason in execute expect

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -3,13 +3,8 @@ import 'package:test/test.dart';
 
 void main() {
   test('the calculator returns 4 when adding 2 and 2', () {
-    // 1st step: setup -> create the calculator object
     final calculator = Calculator();
-
-    //2nd step: side effects -> collect the result you want test
     final result = calculator.add(2, 2);
-
-    //3rd step: expectations -> compare the result against an excpected value
-    expect(result, 4);
+    expect(result, 4.00000, reason: 'It should be exactly 4');
   });
 }
