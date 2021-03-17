1c9f06c643260b1b146a9c7d7f346e724de96ed2
aospiridonov <aospiridonov@gmail.com>
2021-03-17 23:28:21 +0300

    add simple test

--- test/calculator_test.dart
+++ test/calculator_test.dart
@@ -17,4 +17,44 @@ void main() {
     final result = calculator.add(2, 2);
     expect(result, 4.00000, reason: 'It should be exactly 4');
   });
+//
+  test('the calculator returns 4 when adding 2 and 2', () {
+    final calculator = Calculator();
+    expect(calculator.add(2, 2), 4);
+  });
+
+  test('the calculator returns 10 when substract 10 to 20', () {
+    final calculator = Calculator();
+    expect(calculator.substract(20, 10), 10);
+  });
+
+  test('the calculator returns 45 when multiply 5 by 9', () {
+    final calculator = Calculator();
+    expect(calculator.multiply(5, 9), 45);
+  });
+
+  test('the calculator returns 9 when dividing 27 by 3', () {
+    final calculator = Calculator();
+    expect(calculator.divide(27, 3), 9);
+  });
+
+  test('the calculator returns 40 when adding 20 and 20', () {
+    final calculator = Calculator();
+    expect(calculator.add(20, 20), 40);
+  });
+
+  test('the calculator returns -4 when substract 8 to 4', () {
+    final calculator = Calculator();
+    expect(calculator.substract(4, 8), -4);
+  });
+
+  test('the calculator returns 18 when multiply 2 by 9', () {
+    final calculator = Calculator();
+    expect(calculator.multiply(2, 9), 18);
+  });
+
+  test('the calculator returns 1 when dividing 27 by 27', () {
+    final calculator = Calculator();
+    expect(calculator.divide(27, 27), 1);
+  });
 }
